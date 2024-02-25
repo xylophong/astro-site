50193dd818c563b32feedb8dccc5ebda9b8b2bc2
Dinh-Phong Nguyen <dinhphongng@gmail.com>
2024-02-25 14:41:29 +0100

    fix KaTeX implementation

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
-import remarkMath from "remark-math";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import { SITE } from "./src/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
@@ -16,31 +15,10 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    mdx(),
   ],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-      [
-        "remark-math",
-        {
-          // Remark math options
-        },
-      ],
-    ],
-    rehypePlugins: [
-      [
-        "rehype-katex",
-        {
-          // Katex plugin options
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
